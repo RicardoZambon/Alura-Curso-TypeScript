32e8be374e9f2edcd1bb7ee50768d8bd65765a8f
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-06-30 15:56:08 -0300

    1st class, video 09.

--- AluraBank/app/js/models/Negotiation.js
+++ AluraBank/app/js/models/Negotiation.js
@@ -1,27 +1,18 @@
 class Negotiation {
-
-    private _date;
-    private _quantity;
-    private _price;
-
     constructor(date, quantity, price) {
         this._date = date;
         this._quantity = quantity;
         this._price = price;
     }
-
     get date() {
         return this._date;
     }
-
     get quantity() {
         return this._quantity;
     }
-
     get price() {
         return this._price;
     }
-
     get volume() {
         return this._quantity * this._price;
     }
