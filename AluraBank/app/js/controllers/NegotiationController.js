8eaf34580e8c6a94413d9c4e4fe61a937a330025
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-01 18:18:46 -0300

    3rd class, video 01.

--- AluraBank/app/js/controllers/NegotiationController.js
+++ AluraBank/app/js/controllers/NegotiationController.js
@@ -6,7 +6,7 @@ class NegotiationController {
     }
     add(event) {
         event.preventDefault();
-        const negotiation = new Negotiation(this._inputDate.value, this._inputQuantity.value, this._inputPrice.value);
+        const negotiation = new Negotiation(new Date(this._inputDate.value.replace(/-/g, ',')), parseInt(this._inputQuantity.value), parseFloat(this._inputPrice.value));
         console.log(negotiation);
     }
 }
