a8ab68620ab538e19e6e134b34c065551a2e02d3
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-05 21:45:22 -0300

    2nd part, 2nd class, video 03.

--- AluraBank/app/js/views/view.js
+++ AluraBank/app/js/views/view.js
@@ -6,11 +6,19 @@ System.register([], function (exports_1, context_1) {
         setters: [],
         execute: function () {
             View = class View {
-                constructor(selector) {
+                constructor(selector, _scape) {
+                    this._scape = _scape;
                     this._element = $(selector);
+                    if (typeof this._scape == 'undefined') {
+                        this._scape = false;
+                    }
                 }
                 update(model) {
-                    this._element.html(this.template(model));
+                    let template = this.template(model);
+                    if (this._scape) {
+                        template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
+                    }
+                    this._element.html(template);
                 }
             };
             exports_1("View", View);
--- AluraBank/app/ts/views/View.ts
+++ AluraBank/app/ts/views/View.ts
@@ -1,12 +1,22 @@
 export abstract class View<T> {
     protected _element: JQuery;
 
-    constructor(selector: string) {
+    constructor(selector: string, private _scape?: boolean) {
         this._element = $(selector);
+
+        if (typeof this._scape == 'undefined') {
+            this._scape = false;
+        }
     }
 
     update(model: T): void {
-        this._element.html(this.template(model));
+        let template = this.template(model);
+
+        if (this._scape) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
+        }
+
+        this._element.html(template);
     }
 
     abstract template(model: T): string;
