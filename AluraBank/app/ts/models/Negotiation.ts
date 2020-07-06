f527a6dc6d55b1c3ac0b36d5e96e3122265f3e67
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-05 21:35:17 -0300

    2nd part, 2nd class, video 01.

--- AluraBank/app/ts/models/Negotiation.ts
+++ AluraBank/app/ts/models/Negotiation.ts
@@ -1,22 +1,10 @@
 export class Negotiation {
 
-    constructor(private _date: Date, private _quantity: number, private _price: number) {
+    constructor(readonly date: Date, readonly quantity: number, readonly price: number) {
 
-    }
-
-    get date(): Date {
-        return this._date;
-    }
-
-    get quantity(): number {
-        return this._quantity;
-    }
-
-    get price(): number {
-        return this._price;
     }
 
     get volume(): number {
-        return this._quantity * this._price;
+        return this.quantity * this.price;
     }
 }
